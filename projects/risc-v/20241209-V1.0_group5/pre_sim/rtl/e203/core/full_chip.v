f4dde10bb0889311464a29c6c7852ff19f2bf7f7
Albresky <albre02@outlook.com>
2024-12-19 11:01:45 +0800

    [Lab4] Impl wire define and port connections in full_chip.v

--- projects/risc-v/20241209-V1.0_group5/pre_sim/rtl/e203/core/full_chip.v
+++ projects/risc-v/20241209-V1.0_group5/pre_sim/rtl/e203/core/full_chip.v
@@ -1,197 +1,308 @@
-// LAB4: 例化 IO cell 和 e203_soc_top 模块，将 e203_soc_top 模块的所有输入输出外围包一圈 IO
+// LAB4: 实例化 IO 单元和 e203_soc_top 模块，将 e203_soc_top 模块的所有输入输出外围包一圈 IO
 
-e203_soc_top u_e203_soc_top(
-    .hfextclk                           (hfextclk_pin),
-    .hfxoscen                           (hfxoscen_pin),
-    .lfextclk                           (lfextclk_pin),
-    .lfxoscen                           (lfxoscen_pin),
-    .io_pads_jtag_TCK_i_ival            (io_pads_jtag_TCK_i_ival_pin),
-    .io_pads_jtag_TMS_i_ival            (io_pads_jtag_TMS_i_ival_pin),
-    .io_pads_jtag_TDI_i_ival            (io_pads_jtag_TDI_i_ival_pin),
-    .io_pads_jtag_TDO_o_oval            (io_pads_jtag_TDO_o_oval_pin),
-    .io_pads_jtag_TDO_o_oe              (io_pads_jtag_TDO_o_oe_pin),
-    .io_pads_gpioA_i_ival               (io_pads_gpioA_i_ival_pin),
-    .io_pads_gpioA_o_oval               (io_pads_gpioA_o_oval_pin),
-    .io_pads_gpioA_o_oe                 (io_pads_gpioA_o_oe _pin),
-    .io_pads_gpioB_i_ival               (io_pads_gpioB_i_ival_pin),
-    .io_pads_gpioB_o_oval               (io_pads_gpioB_o_oval_pin),
-    .io_pads_gpioB_o_oe                 (io_pads_gpioB_o_oe_pin),
-    .io_pads_qspi0_sck_o_oval           (io_pads_qspi0_sck_o_oval_pin),
-    .io_pads_qspi0_cs_0_o_oval          (io_pads_qspi0_cs_0_o_oval_pin),
-    .io_pads_qspi0_dq_0_i_ival          (io_pads_qspi0_dq_0_i_ival_pin),
-    .io_pads_qspi0_dq_0_o_oval          (io_pads_qspi0_dq_0_o_oval_pin),
-    .io_pads_qspi0_dq_0_o_oe            (io_pads_qspi0_dq_0_o_oe_pin),
-    .io_pads_qspi0_dq_1_i_ival          (io_pads_qspi0_dq_1_i_ival_pin),
-    .io_pads_qspi0_dq_1_o_oval          (io_pads_qspi0_dq_1_o_oval_pin),
-    .io_pads_qspi0_dq_1_o_oe            (io_pads_qspi0_dq_1_o_oe_pin),
-    .io_pads_qspi0_dq_2_i_ival          (io_pads_qspi0_dq_2_i_ival_pin),
-    .io_pads_qspi0_dq_2_o_oval          (io_pads_qspi0_dq_2_o_oval_pin),
-    .io_pads_qspi0_dq_2_o_oe            (io_pads_qspi0_dq_2_o_oe_pin),
-    .io_pads_qspi0_dq_3_i_ival          (io_pads_qspi0_dq_3_i_ival_pin),
-    .io_pads_qspi0_dq_3_o_oval          (io_pads_qspi0_dq_3_o_oval_pin),
-    .io_pads_qspi0_dq_3_o_oe            (io_pads_qspi0_dq_3_o_oe_pin),
-    .io_pads_aon_erst_n_i_ival          (io_pads_aon_erst_n_i_ival_pin),
-    .io_pads_dbgmode0_n_i_ival          (io_pads_dbgmode0_n_i_ival       _pin),
-    .io_pads_dbgmode1_n_i_ival          (io_pads_dbgmode1_n_i_ival       _pin),
-    .io_pads_dbgmode2_n_i_ival          (io_pads_dbgmode2_n_i_ival       _pin),
-    .io_pads_bootrom_n_i_ival           (io_pads_bootrom_n_i_ival      _pin),
-    .io_pads_aon_pmu_dwakeup_n_i_ival   (io_pads_aon_pmu_dwakeup_n_i_ival_pin),
-    .io_pads_aon_pmu_padrst_o_oval      (io_pads_aon_pmu_padrst_o_oval_pin),
-    .io_pads_aon_pmu_vddpaden_o_oval    (io_pads_aon_pmu_vddpaden_o_oval_pin)
+module full_chip(
+    // 晶体管高速时钟（16MHz）
+    input  hfextclk,
+    // 晶体管高速时钟使能信号
+    output hfxoscen, 
+
+    // 晶体管低速时钟（32.768KHz）
+    input  lfextclk,
+    // 晶体管低速时钟使能信号
+    output lfxoscen,
+
+    // JTAG TCK 输入，需上拉
+    input   io_pads_jtag_TCK_i_ival,
+
+    // JTAG TMS 输入，需上拉
+    input   io_pads_jtag_TMS_i_ival,
+
+    // JTAG TDI 输入，需上拉
+    input   io_pads_jtag_TDI_i_ival,
+
+    // JTAG TDO 输出，有使能
+    output  io_pads_jtag_TDO_o,
+
+    // GPIO 双向端口，有使能
+    input  [31:0] io_pads_gpioA,
+    input  [31:0] io_pads_gpioB,
+
+    // QSPI0 SCK 和 CS 输出，无使能
+    output  io_pads_qspi0_sck_o_oval,
+    output  io_pads_qspi0_cs_0_o_oval,
+
+    // QSPI0 DQ 双向 I/O，有使能，需上拉
+    input   io_pads_qspi0_dq_0,
+    input   io_pads_qspi0_dq_1,
+    input   io_pads_qspi0_dq_2,
+    input   io_pads_qspi0_dq_3,
+  
+    // Erst 输入，需上拉
+    input   io_pads_aon_erst_n_i_ival,
+
+    // 调试模式输入，需上拉
+    input  io_pads_dbgmode0_n_i_ival,
+    input  io_pads_dbgmode1_n_i_ival,
+    input  io_pads_dbgmode2_n_i_ival,
+
+    // BootRom 输入，需上拉
+    input  io_pads_bootrom_n_i_ival,
+
+    // 唤醒输入，需上拉
+    input  io_pads_aon_pmu_dwakeup_n_i_ival,
+
+    // PMU 输出，无使能
+    output io_pads_aon_pmu_padrst_o_oval,
+    output io_pads_aon_pmu_vddpaden_o_oval 
 );
 
-// 1. hfextclk: Input pad
-PI u_pad_hfextclk (
-    .PAD(hfextclk),
-    .C(hfextclk_pin)
-);
+    wire hfextclk_pin;
+    wire hfxoscen_pin;
+    wire lfextclk_pin;
+    wire lfxoscen_pin;
+    wire io_pads_jtag_TCK_i_ival_pin;
+    wire io_pads_jtag_TMS_i_ival_pin;
+    wire io_pads_jtag_TDI_i_ival_pin;
+    wire io_pads_jtag_TDO_o_oval_pin;
+    wire io_pads_jtag_TDO_o_oe_pin;
+    wire [31:0] io_pads_gpioA_i_ival_pin;
+    wire [31:0] io_pads_gpioA_o_oval_pin;
+    wire [31:0] io_pads_gpioA_o_oe_pin;
+    wire [31:0] io_pads_gpioB_i_ival_pin;
+    wire [31:0] io_pads_gpioB_o_oval_pin;
+    wire [31:0] io_pads_gpioB_o_oe_pin;
+    wire io_pads_qspi0_sck_o_oval_pin;
+    wire io_pads_qspi0_cs_0_o_oval_pin;
+    wire io_pads_qspi0_dq_0_i_ival_pin;
+    wire io_pads_qspi0_dq_0_o_oval_pin;
+    wire io_pads_qspi0_dq_0_o_oe_pin;
+    wire io_pads_qspi0_dq_1_i_ival_pin;
+    wire io_pads_qspi0_dq_1_o_oval_pin;
+    wire io_pads_qspi0_dq_1_o_oe_pin;
+    wire io_pads_qspi0_dq_2_i_ival_pin;
+    wire io_pads_qspi0_dq_2_o_oval_pin;
+    wire io_pads_qspi0_dq_2_o_oe_pin;
+    wire io_pads_qspi0_dq_3_i_ival_pin;
+    wire io_pads_qspi0_dq_3_o_oval_pin;
+    wire io_pads_qspi0_dq_3_o_oe_pin;
+    wire io_pads_aon_erst_n_i_ival_pin;
+    wire io_pads_dbgmode0_n_i_ival_pin;
+    wire io_pads_dbgmode1_n_i_ival_pin;
+    wire io_pads_dbgmode2_n_i_ival_pin;
+    wire io_pads_bootrom_n_i_ival_pin;
+    wire io_pads_aon_pmu_dwakeup_n_i_ival_pin;
+    wire io_pads_aon_pmu_padrst_o_oval_pin;
+    wire io_pads_aon_pmu_vddpaden_o_oval_pin;
 
-// 1) hfxoscen: Output pad
-PO4 u_pad_hfxoscen (
-    .I(hfxoscen_pin),
-    .PAD(hfxoscen)
-);
+    ///////////////////////////////////////////
+    ///////// 实例化 e203_soc_top 模块 /////////
+    ///////////////////////////////////////////
+    e203_soc_top u_e203_soc_top(
+        .hfextclk                           (hfextclk_pin),
+        .hfxoscen                           (hfxoscen_pin),
+        .lfextclk                           (lfextclk_pin),
+        .lfxoscen                           (lfxoscen_pin),
+        .io_pads_jtag_TCK_i_ival            (io_pads_jtag_TCK_i_ival_pin),
+        .io_pads_jtag_TMS_i_ival            (io_pads_jtag_TMS_i_ival_pin),
+        .io_pads_jtag_TDI_i_ival            (io_pads_jtag_TDI_i_ival_pin),
+        .io_pads_jtag_TDO_o_oval            (io_pads_jtag_TDO_o_oval_pin),
+        .io_pads_jtag_TDO_o_oe              (io_pads_jtag_TDO_o_oe_pin),
+        .io_pads_gpioA_i_ival               (io_pads_gpioA_i_ival_pin),
+        .io_pads_gpioA_o_oval               (io_pads_gpioA_o_oval_pin),
+        .io_pads_gpioA_o_oe                 (io_pads_gpioA_o_oe_pin),
+        .io_pads_gpioB_i_ival               (io_pads_gpioB_i_ival_pin),
+        .io_pads_gpioB_o_oval               (io_pads_gpioB_o_oval_pin),
+        .io_pads_gpioB_o_oe                 (io_pads_gpioB_o_oe_pin),
+        .io_pads_qspi0_sck_o_oval           (io_pads_qspi0_sck_o_oval_pin),
+        .io_pads_qspi0_cs_0_o_oval          (io_pads_qspi0_cs_0_o_oval_pin),
+        .io_pads_qspi0_dq_0_i_ival          (io_pads_qspi0_dq_0_i_ival_pin),
+        .io_pads_qspi0_dq_0_o_oval          (io_pads_qspi0_dq_0_o_oval_pin),
+        .io_pads_qspi0_dq_0_o_oe            (io_pads_qspi0_dq_0_o_oe_pin),
+        .io_pads_qspi0_dq_1_i_ival          (io_pads_qspi0_dq_1_i_ival_pin),
+        .io_pads_qspi0_dq_1_o_oval          (io_pads_qspi0_dq_1_o_oval_pin),
+        .io_pads_qspi0_dq_1_o_oe            (io_pads_qspi0_dq_1_o_oe_pin),
+        .io_pads_qspi0_dq_2_i_ival          (io_pads_qspi0_dq_2_i_ival_pin),
+        .io_pads_qspi0_dq_2_o_oval          (io_pads_qspi0_dq_2_o_oval_pin),
+        .io_pads_qspi0_dq_2_o_oe            (io_pads_qspi0_dq_2_o_oe_pin),
+        .io_pads_qspi0_dq_3_i_ival          (io_pads_qspi0_dq_3_i_ival_pin),
+        .io_pads_qspi0_dq_3_o_oval          (io_pads_qspi0_dq_3_o_oval_pin),
+        .io_pads_qspi0_dq_3_o_oe            (io_pads_qspi0_dq_3_o_oe_pin),
+        .io_pads_aon_erst_n_i_ival          (io_pads_aon_erst_n_i_ival_pin),
+        .io_pads_dbgmode0_n_i_ival          (io_pads_dbgmode0_n_i_ival_pin),
+        .io_pads_dbgmode1_n_i_ival          (io_pads_dbgmode1_n_i_ival_pin),
+        .io_pads_dbgmode2_n_i_ival          (io_pads_dbgmode2_n_i_ival_pin),
+        .io_pads_bootrom_n_i_ival           (io_pads_bootrom_n_i_ival_pin),
+        .io_pads_aon_pmu_dwakeup_n_i_ival   (io_pads_aon_pmu_dwakeup_n_i_ival_pin),
+        .io_pads_aon_pmu_padrst_o_oval      (io_pads_aon_pmu_padrst_o_oval_pin),
+        .io_pads_aon_pmu_vddpaden_o_oval    (io_pads_aon_pmu_vddpaden_o_oval_pin)
+    );
 
-// 2) lfextclk: Input pad
-PI u_pad_lfextclk (
-    .PAD(lfextclk),
-    .C(lfextclk_pin)
-);
+    ////////////////////////////////
+    //////// 实例化 IO 单元 /////////
+    ////////////////////////////////
+    // hfextclk: 输入端口
+    PI u_pad_hfextclk (
+        .PAD(hfextclk),
+        .C(hfextclk_pin)
+    );
 
-// lfxoscen: Output pad
-PO4 u_pad_lfxoscen (
-    .I(lfxoscen_pin),
-    .PAD(lfxoscen)
-);
+    // hfxoscen: 输出端口
+    PO4 u_pad_hfxoscen (
+        .I(hfxoscen_pin),
+        .PAD(hfxoscen)
+    );
 
-// 3) io_pads_jtag_TCK_i_ival: Input pad with pull-up
-PIU u_pad_io_pads_jtag_TCK_i_ival (
-    .PAD(io_pads_jtag_TCK_i_ival),
-    .C(io_pads_jtag_TCK_i_ival_pin)
-);
+    // lfextclk: 输入端口
+    PI u_pad_lfextclk (
+        .PAD(lfextclk),
+        .C(lfextclk_pin)
+    );
 
-// 4) io_pads_jtag_TMS_i_ival and io_pads_jtag_TDI_i_ival: Input pads with pull-up
-PIU u_pad_io_pads_jtag_TMS_i_ival (
-    .PAD(io_pads_jtag_TMS_i_ival),
-    .C(io_pads_jtag_TMS_i_ival_pin)
-);
+    // lfxoscen: 输出端口
+    PO4 u_pad_lfxoscen (
+        .I(lfxoscen_pin),
+        .PAD(lfxoscen)
+    );
 
-PIU u_pad_io_pads_jtag_TDI_i_ival (
-    .PAD(io_pads_jtag_TDI_i_ival),
-    .C(io_pads_jtag_TDI_i_ival_pin)
-);
+    // io_pads_jtag_TCK_i_ival: 输入端口，需上拉
+    PIU u_pad_io_pads_jtag_TCK_i_ival (
+        .PAD(io_pads_jtag_TCK_i_ival),
+        .C(io_pads_jtag_TCK_i_ival_pin)
+    );
 
-// 5) io_pads_jtag_TDO_o_oval and io_pads_jtag_TDO_o_oe: Tri-state output
-POT16 u_pad_io_pads_jtag_TDO_o (
-    .OEN(~io_pads_jtag_TDO_o_oe_pin),
-    .I(io_pads_jtag_TDO_o_oval_pin),
-    .PAD(io_pads_jtag_TDO_o)
-);
+    // io_pads_jtag_TMS_i_ival 和 io_pads_jtag_TDI_i_ival: 输入端口，需上拉
+    PIU u_pad_io_pads_jtag_TMS_i_ival (
+        .PAD(io_pads_jtag_TMS_i_ival),
+        .C(io_pads_jtag_TMS_i_ival_pin)
+    );
 
-// 6) io_pads_gpioA: Bi-directional port
-PB8 u_pad_gpioAX (
-    .OEN(~io_pads_gpioA_o_oe_pin[X]),
-    .C(io_pads_gpioA_i_ival_pin[X]),
-    .PAD(io_pads_gpioA[X]),
-    .I(io_pads_gpioA_o_oval_pin[X])
-);
+    PIU u_pad_io_pads_jtag_TDI_i_ival (
+        .PAD(io_pads_jtag_TDI_i_ival),
+        .C(io_pads_jtag_TDI_i_ival_pin)
+    );
 
-// 7) io_pads_gpioB: Bi-directional port
-PB8 u_pad_gpioBX (
-    .OEN(~io_pads_gpioB_o_oe_pin[X]),
-    .C(io_pads_gpioB_i_ival_pin[X]),
-    .PAD(io_pads_gpioB[X]),
-    .I(io_pads_gpioB_o_oval_pin[X])
-);
+    // io_pads_jtag_TDO_o_oval 和 io_pads_jtag_TDO_o_oe: 三态输出
+    POT16 u_pad_io_pads_jtag_TDO_o (
+        .OEN(~io_pads_jtag_TDO_o_oe_pin),
+        .I(io_pads_jtag_TDO_o_oval_pin),
+        .PAD(io_pads_jtag_TDO_o)
+    );
 
-// 8) io_pads_qspi0_sck_o_oval: Output pad
-PO16 u_pad_io_pads_qspi0_sck_o_oval (
-    .I(io_pads_qspi0_sck_o_oval_pin),
-    .PAD(io_pads_qspi0_sck_o_oval)
-);
+    // io_pads_gpioA: 双向端口
+    genvar i;
+    generate
+        for (i = 0; i < 32; i = i + 1) begin : gpioA
+            PB8 u_pad_gpioAX (
+                .OEN(~io_pads_gpioA_o_oe_pin[i]),
+                .C(io_pads_gpioA_i_ival_pin[i]),
+                .PAD(io_pads_gpioA[i]),
+                .I(io_pads_gpioA_o_oval_pin[i])
+            );
+        end
+    endgenerate
 
-// 9) io_pads_qspi0_cs_0_o_oval: Output pad
-PO16 u_pad_io_pads_qspi0_cs_0_o_oval (
-    .I(io_pads_qspi0_cs_0_o_oval_pin),
-    .PAD(io_pads_qspi0_cs_0_o_oval)
-);
+    // io_pads_gpioB: 双向端口
+    generate
+        for (i = 0; i < 32; i = i + 1) begin : gpioB
+            PB8 u_pad_gpioBX (
+                .OEN(~io_pads_gpioB_o_oe_pin[i]),
+                .C(io_pads_gpioB_i_ival_pin[i]),
+                .PAD(io_pads_gpioB[i]),
+                .I(io_pads_gpioB_o_oval_pin[i])
+            );
+        end
+    endgenerate
 
-// 10) io_pads_qspi0_dq_0: Bi-directional port
-PBCU16 u_pad_io_pads_qspi0_dq_0 (
-    .PAD(io_pads_qspi0_dq_0),
-    .OEN(~io_pads_qspi0_dq_0_o_oe_pin),
-    .REN(1'b0),
-    .I(io_pads_qspi0_dq_0_o_oval_pin),
-    .C(io_pads_qspi0_dq_0_i_ival_pin)
-);
+    // io_pads_qspi0_sck_o_oval: 输出端口
+    PO16 u_pad_io_pads_qspi0_sck_o_oval (
+        .I(io_pads_qspi0_sck_o_oval_pin),
+        .PAD(io_pads_qspi0_sck_o_oval)
+    );
 
-// 11) io_pads_qspi0_dq_1, io_pads_qspi0_dq_2, io_pads_qspi0_dq_3: Bi-directional ports
-PBCU16 u_pad_io_pads_qspi0_dq_1 (
-    .PAD(io_pads_qspi0_dq_1),
-    .OEN(~io_pads_qspi0_dq_1_o_oe_pin),
-    .REN(1'b0),
-    .I(io_pads_qspi0_dq_1_o_oval_pin),
-    .C(io_pads_qspi0_dq_1_i_ival_pin)
-);
+    // io_pads_qspi0_cs_0_o_oval: 输出端口
+    PO16 u_pad_io_pads_qspi0_cs_0_o_oval (
+        .I(io_pads_qspi0_cs_0_o_oval_pin),
+        .PAD(io_pads_qspi0_cs_0_o_oval)
+    );
 
-PBCU16 u_pad_io_pads_qspi0_dq_2 (
-    .PAD(io_pads_qspi0_dq_2),
-    .OEN(~io_pads_qspi0_dq_2_o_oe_pin),
-    .REN(1'b0),
-    .I(io_pads_qspi0_dq_2_o_oval_pin),
-    .C(io_pads_qspi0_dq_2_i_ival_pin)
-);
+    // io_pads_qspi0_dq_0: 双向端口
+    PBCU16 u_pad_io_pads_qspi0_dq_0 (
+        .PAD(io_pads_qspi0_dq_0),
+        .OEN(~io_pads_qspi0_dq_0_o_oe_pin),
+        .REN(1'b0),
+        .I(io_pads_qspi0_dq_0_o_oval_pin),
+        .C(io_pads_qspi0_dq_0_i_ival_pin)
+    );
 
-PBCU16 u_pad_io_pads_qspi0_dq_3 (
-    .PAD(io_pads_qspi0_dq_3),
-    .OEN(~io_pads_qspi0_dq_3_o_oe_pin),
-    .REN(1'b0),
-    .I(io_pads_qspi0_dq_3_o_oval_pin),
-    .C(io_pads_qspi0_dq_3_i_ival_pin)
-);
+    // io_pads_qspi0_dq_1, io_pads_qspi0_dq_2, io_pads_qspi0_dq_3: 双向端口
+    PBCU16 u_pad_io_pads_qspi0_dq_1 (
+        .PAD(io_pads_qspi0_dq_1),
+        .OEN(~io_pads_qspi0_dq_1_o_oe_pin),
+        .REN(1'b0),
+        .I(io_pads_qspi0_dq_1_o_oval_pin),
+        .C(io_pads_qspi0_dq_1_i_ival_pin)
+    );
 
-// 12) io_pads_aon_erst_n_i_ival: Input pad with pull-up
-PIU u_pad_io_pads_aon_erst_n_i_ival (
-    .PAD(io_pads_aon_erst_n_i_ival),
-    .C(io_pads_aon_erst_n_i_ival_pin)
-);
+    PBCU16 u_pad_io_pads_qspi0_dq_2 (
+        .PAD(io_pads_qspi0_dq_2),
+        .OEN(~io_pads_qspi0_dq_2_o_oe_pin),
+        .REN(1'b0),
+        .I(io_pads_qspi0_dq_2_o_oval_pin),
+        .C(io_pads_qspi0_dq_2_i_ival_pin)
+    );
 
-// 13) io_pads_dbgmode0_n_i_ival: Input pad with pull-up
-PIU u_pad_io_pads_dbgmode0_n_i_ival (
-    .PAD(io_pads_dbgmode0_n_i_ival),
-    .C(io_pads_dbgmode0_n_i_ival_pin)
-);
+    PBCU16 u_pad_io_pads_qspi0_dq_3 (
+        .PAD(io_pads_qspi0_dq_3),
+        .OEN(~io_pads_qspi0_dq_3_o_oe_pin),
+        .REN(1'b0),
+        .I(io_pads_qspi0_dq_3_o_oval_pin),
+        .C(io_pads_qspi0_dq_3_i_ival_pin)
+    );
 
-// 14) io_pads_dbgmode1_n_i_ival and io_pads_dbgmode2_n_i_ival: Input pads with pull-up
-PIU u_pad_io_pads_dbgmode1_n_i_ival (
-    .PAD(io_pads_dbgmode1_n_i_ival),
-    .C(io_pads_dbgmode1_n_i_ival_pin)
-);
+    // io_pads_aon_erst_n_i_ival: 输入端口，需上拉
+    PIU u_pad_io_pads_aon_erst_n_i_ival (
+        .PAD(io_pads_aon_erst_n_i_ival),
+        .C(io_pads_aon_erst_n_i_ival_pin)
+    );
 
-PIU u_pad_io_pads_dbgmode2_n_i_ival (
-    .PAD(io_pads_dbgmode2_n_i_ival),
-    .C(io_pads_dbgmode2_n_i_ival_pin)
-);
+    // io_pads_dbgmode0_n_i_ival: 输入端口，需上拉
+    PIU u_pad_io_pads_dbgmode0_n_i_ival (
+        .PAD(io_pads_dbgmode0_n_i_ival),
+        .C(io_pads_dbgmode0_n_i_ival_pin)
+    );
 
-// 15) io_pads_bootrom_n_i_ival: Input pad with pull-up
-PIU u_pad_io_pads_bootrom_n_i_ival (
-    .PAD(io_pads_bootrom_n_i_ival),
-    .C(io_pads_bootrom_n_i_ival_pin)
-);
+    // io_pads_dbgmode1_n_i_ival 和 io_pads_dbgmode2_n_i_ival: 输入端口，需上拉
+    PIU u_pad_io_pads_dbgmode1_n_i_ival (
+        .PAD(io_pads_dbgmode1_n_i_ival),
+        .C(io_pads_dbgmode1_n_i_ival_pin)
+    );
 
-// 16) u_pad_io_pads_aon_pmu_dwakeup_n_i_ival: Input pad with pull-up
-PIU u_pad_io_pads_aon_pmu_dwakeup_n_i_ival (
-    .PAD(io_pads_aon_pmu_dwakeup_n_i_ival),
-    .C(io_pads_aon_pmu_dwakeup_n_i_ival_pin)
-);
+    PIU u_pad_io_pads_dbgmode2_n_i_ival (
+        .PAD(io_pads_dbgmode2_n_i_ival),
+        .C(io_pads_dbgmode2_n_i_ival_pin)
+    );
 
-// 17) io_pads_aon_pmu_padrst_o_oval and io_pads_aon_pmu_vddpaden_o_oval: Output pads
-PO16 u_pad_io_pads_aon_pmu_padrst_o_oval (
-    .I(io_pads_aon_pmu_padrst_o_oval_pin),
-    .PAD(io_pads_aon_pmu_padrst_o_oval)
-);
+    // io_pads_bootrom_n_i_ival: 输入端口，需上拉
+    PIU u_pad_io_pads_bootrom_n_i_ival (
+        .PAD(io_pads_bootrom_n_i_ival),
+        .C(io_pads_bootrom_n_i_ival_pin)
+    );
 
-PO16 u_pad_io_pads_aon_pmu_vddpaden_o_oval (
-    .I(io_pads_aon_pmu_vddpaden_o_oval_pin),
-    .PAD(io_pads_aon_pmu_vddpaden_o_oval)
-);
+    // io_pads_aon_pmu_dwakeup_n_i_ival: 输入端口，需上拉
+    PIU u_pad_io_pads_aon_pmu_dwakeup_n_i_ival (
+        .PAD(io_pads_aon_pmu_dwakeup_n_i_ival),
+        .C(io_pads_aon_pmu_dwakeup_n_i_ival_pin)
+    );
+
+    // io_pads_aon_pmu_padrst_o_oval 和 io_pads_aon_pmu_vddpaden_o_oval: 输出端口
+    PO16 u_pad_io_pads_aon_pmu_padrst_o_oval (
+        .I(io_pads_aon_pmu_padrst_o_oval_pin),
+        .PAD(io_pads_aon_pmu_padrst_o_oval)
+    );
+
+    PO16 u_pad_io_pads_aon_pmu_vddpaden_o_oval (
+        .I(io_pads_aon_pmu_vddpaden_o_oval_pin),
+        .PAD(io_pads_aon_pmu_vddpaden_o_oval)
+    );
+
+endmodule
